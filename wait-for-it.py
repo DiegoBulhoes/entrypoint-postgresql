c63bd0662fc07ab0a5cf4539394cab6f5aa09328
DiegoBulhoes <diegobulhoesm@gmail.com>
2020-01-25 16:36:33 -0400

    Update the configuration mode to run the script

--- wait-for-it.py
+++ wait-for-it.py
@@ -51,21 +51,23 @@ def timeout(tic: float, toc: float):
         boolean -- Returns true if the timeout was not given,
                     otherwise false will be returned.
     """
+    print(toc-tic)
+    print(os.environ['CONNECT_TIMEOUT'])
     return (toc-tic) > int(os.environ['CONNECT_TIMEOUT'])
 
 
-def is_server_up(env_file_path: str, tic: float, toc: float, check_timeout: float):
+def is_server_up(env_file_path: str, tic: float):
     """Check if the service is running.
 
     Arguments:
         env_file_path {str} -- environment variable file path.
         tic {float} -- start time a stopwatch.
-        toc {float} -- end time a stopwatch.
 
     Raises:
         Exception: Exception issued when the service is not operational before
                     the time specified by the user
     """
+
     while True:
         try:
             load_environment(env_file_path)
@@ -80,9 +82,10 @@ def is_server_up(env_file_path: str, tic: float, toc: float, check_timeout: floa
         except BaseException:
             print('Unexpected error')
         finally:
+            toc = time.time()
             if timeout(tic, toc):
                 raise Exception("Timeout")
-            sleep(check_timeout)
+            sleep(int(os.environ['CHECK_SERVICE']))
 
 
 def helpInit():
@@ -90,17 +93,10 @@ def helpInit():
     parser = arg_parse(
         description='Script to perform a PostgreSQL service health check')
     parser.add_argument('-p', '--path', help='path file', required=True)
-    parser.add_argument('-t', '--timeout', type=float,
-                        help='timeout value', default=10, required=True)
-    parser.add_argument('-c', '--check-timeout', type=float,
-                        help='Time between attempts to check the \
-                            PostgreSQL service', default=10, required=True)
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = helpInit()
     tic = time.time()
-    toc = tic + args.timeout
-    is_server_up(env_file_path=args.path, tic=tic,
-                 toc=toc, check_timeout=args.check_timeout)
+    is_server_up(env_file_path=args.path, tic=tic)
